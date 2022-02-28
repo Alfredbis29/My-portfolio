bee9313617b1c59658647bb2023f873f16e8dda4
Alfred <kalumunabisimwa5@gmail.com>
2022-02-28 16:36:46 +0200

    Add all sections and create an array to load all sections in humbeger

--- js/script.js
+++ js/script.js
@@ -25,3 +25,74 @@ function toggle() {
     isOpen = false;
   }
 }
+Array.from(links).forEach((link) => {
+  link.addEventListener("click", () => {
+    header.classList.remove("open");
+    headline.classList.remove("bur-effect");
+  });
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 600) {
+    header.classList.remove("open");
+    headline.classList.remove("bur-effect");
+  }
+});
+
+menuButtonOpen.addEventListener("click", toggle);
+menuButtonClose.addEventListener("click", toggle);
+
+const form = document.getElementById("contact-form");
+const email = document.getElementById("email");
+const errorMess = document.querySelector("small");
+const userMessage = document.getElementById("message");
+const userName = document.getElementById("name");
+const submitBtn = document.querySelector("#submit");
+const message = [];
+
+[userName, email, userName].forEach((item) => {
+  item.addEventListener("input", () => {
+    const data = {
+      name: userName.value,
+      email: email.value,
+      message: userMessage.value,
+    };
+    const dataStored = JSON.stringify(data);
+    localStorage.setItem("data", dataStored);
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  const emailValue = email.value;
+  if (emailValue === "") {
+    message.push("The Email must not be empty");
+  }
+  if (emailValue !== emailValue.toLowerCase()) {
+    message.push("The email must be in lowercase");
+  }
+  if (message.length > 0) {
+    e.preventDefault();
+    errorMess.innerText = message.join(",");
+    errorMess.classList.add("show-message");
+  }
+});
+window.addEventListener("load", (e) => {
+  const getData = JSON.parse(localStorage.getItem("data"));
+  userName.value = getData.name;
+  email.value = getData.email;
+  userMessage.value = getData.message;
+  if (e.target.value !== e.target.value.toLowerCase()) {
+    submitBtn.classList.add("disabled");
+    submitBtn.classList.remove("hover");
+  }
+});
+
+email.addEventListener("input", (e) => {
+  if (e.target.value !== e.target.value.toLowerCase()) {
+    submitBtn.classList.add("disabled");
+    submitBtn.classList.remove("hover");
+  } else {
+    submitBtn.classList.remove("disabled");
+    submitBtn.classList.add("hover");
+  }
+});
