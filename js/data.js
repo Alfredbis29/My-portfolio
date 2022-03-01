6a887748fc47525b814c0469e90fb9870d052365
Alfred <kalumunabisimwa5@gmail.com>
2022-03-01 15:38:24 +0200

    Add object and it poperties

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ const data = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     detail:
       "AlfaCamp is not simply a series of unconnected stories, but is in fact a movement that can change the lives of millions of children.",
-    technologies: ["Html", "Css", "Javascript"],
+    technologies: ["Html", "Css", "Javascript , bootstrap"],
     liveDemo: "",
     sourceCode: "",
   },
