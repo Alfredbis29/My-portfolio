ccb444949165f13e4423dc0c1bde4b0790626997
Monika-Soam17 <86161447+Monika-Soam17@users.noreply.github.com>
2022-03-01 18:49:55 +0530

    Updated array

--- js/data.js
+++ js/data.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-tabs */
-const data = [
-  {
+const data = [{
     id: 2,
     image: "images/elvivo.jpeg",
     name: "AlfaCamp",
@@ -9,10 +8,8 @@ const data = [
       interface: "Frontend Dev",
       year: 2015,
     },
-    fullDesciption:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    detail:
-      "AlfaCamp is not simply a series of unconnected stories, but is in fact a movement that can change the lives of millions of children.",
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: "AlfaCamp is not simply a series of unconnected stories, but is in fact a movement that can change the lives of millions of children.",
     technologies: ["Html", "Css", "Javascript"],
     liveDemo: "",
     sourceCode: "",
@@ -26,11 +23,9 @@ const data = [
       interface: "Backend Dev",
       year: 2015,
     },
-    fullDesciption:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    detail:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    technologies: ["Html", "Css", "Javascript", "github", "Ruby", "Bootstrap"],
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    technologies: ["Html", "Css", "Javascript"],
     liveDemo: "",
     sourceCode: "",
   },
@@ -43,10 +38,8 @@ const data = [
       interface: "Backend Dev",
       year: 2015,
     },
-    fullDesciption:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    detail:
-      "Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    fullDesciption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    detail: "Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     technologies: ["Html", "Css", "Javascript", "github", "Ruby", "Bootstrap"],
     liveDemo: "",
     sourceCode: "",
@@ -60,10 +53,8 @@ const data = [
       interface: "Backend Dev",
       year: 2015,
     },
-    fullDesciption:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ",
-    detail:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    fullDesciption: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae beatae numquam vitae modi est quod laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi voluptatum quibusdam adipisci voluptates magnam ipsam dolores ad! Adipisci veritatis, sint perspiciatis reiciendis omnis, maiores incidunt, ex aut inventore deleniti nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia deleniti nobis facilis delectus consequuntur, culpa commodi quia repellendus molestias doloribus praesentium voluptas nisi sint, similique repellat atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ",
+    detail: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     technologies: ["Html", "Css", "Javascript", "github", "Ruby", "Bootstrap"],
     liveDemo: "",
     sourceCode: "",
