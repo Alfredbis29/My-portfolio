2907214ae271a0489338a0f60db121f8cb8b6e38
Alfred <kalumunabisimwa5@gmail.com>
2022-03-11 14:11:35 +0200

    Add formspree

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ const data = [
       year: 2022,
     },
     fullDesciption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      'AlfaCamp is not simply a series of unconnected stories, but is in fact a movement that can change the lives of millions of children.',
     detail:
       'AlfaCamp is not simply a series of unconnected stories, but is in fact a movement that can change the lives of millions of children.',
     technologies: ['Html', 'Css', 'Javascript', 'bootstrap'],
